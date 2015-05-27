849365aaae577c3f1b1c9fd1f770f3f36d475be6
sebbrochet <contact@sebbrochet.com>
2015-05-27 23:00:04 +0200

    Replaced Symfony Standard template with Symfony demo as application

--- application/app/AppKernel.php
+++ application/app/AppKernel.php
@@ -7,6 +7,10 @@ class AppKernel extends Kernel
 {
     public function registerBundles()
     {
+        // When you install a third-party bundle or create a new bundle in your
+        // application, you must add it in the following array to register it
+        // in the application. Otherwise, the bundle won't be enabled and you
+        // won't be able to use it.
         $bundles = array(
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
@@ -16,12 +20,16 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new AppBundle\AppBundle(),
         );
 
+        // Some bundles are only used while developing the application or during
+        // the unit and functional tests. Therefore, they are only registered
+        // when the application runs in 'dev' or 'test' environments. This allows
+        // to increase application performance in the production environment.
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
-            $bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
@@ -32,6 +40,6 @@ class AppKernel extends Kernel
 
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-        $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
+        $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
     }
 }
